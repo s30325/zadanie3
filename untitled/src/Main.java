3f6988fe8964a07263ca16450068b03987cd2ebd
Wojciech Chelkowski <s30325@pjwstk.edu.pl>
2024-03-27 13:33:45 +0100

    zadanie3

---
+++ untitled/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Adam", "Nowacki", "s21357", "s21357@pjwstk.edu.pl",
+                ".pjwstk.edu.pl", new double[]{3.0, 5.0, 2.0, 3.5});
+        Student student2 = new Student("Wojciech", "Chelkowski", "s54367", "s54367@pjwstk.edu.pl",
+                ".pjwstk.edu.pl", new double[]{3.0, 3.5, 4.0, 3.0});
+        Student student3 = new Student("Pavlo", "Valerko", "s98457", "s98457@pjwstk.edu.pl",
+                ".pjwstk.edu.pl", new double[]{5.0, 5.0, 3.0, 3.0});
+        Student student4 = new Student("Jan", "Kowalski", "s29845", "s29845@pjwstk.edu.pl",
+                ".pjwstk.edu.pl", new double[]{5.0, 5.0, 2.0, 3.5});
+        Student student5 = new Student("Anna", "Zyt", "s39820", "s39820@pjwstk.edu.pl",
+                ".pjwstk.edu.pl", new double[]{3.0, 5.0, 4.0, 3.0});
+
+        StudentGroup studentGroup = new StudentGroup("group1", new Student[]{student1, student2, student3,
+                student4});
+        System.out.println("Srednia studenta");
+        System.out.println(student1.countAGP());
+        studentGroup.addStudent(student5);
+        System.out.println("liczba studentow w grupie");
+        System.out.println(studentGroup.students.length);
+        for(int i = 0; i < studentGroup.students.length; i++) {
+            System.out.println(studentGroup.students[i]);
+        }
+    }
+}
---
+++ untitled/src/StudentGroup.java
@@ -0,0 +1,43 @@
+public class StudentGroup {
+    public String name;
+    public Student[] students;
+
+    public StudentGroup(String name, Student[] students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public boolean isInGroup(Student student) {
+        for (Student i : students) {
+            if (i == student) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public Student[] addStudent(Student student) {
+        Student[] studentsNew = new Student[students.length];
+        try {
+            if(students.length < 15 && !isInGroup(student)) {
+                studentsNew = new Student[students.length + 1];
+                for(int i = 0; i < students.length; i++) {
+                    studentsNew[i] = students[i];
+                }
+                studentsNew[students.length-1] = student;
+                students = studentsNew;
+            }
+        } catch (IllegalStateException e) {
+            if (students.length >= 15) {
+                System.out.println("za duzo studentow w grupie");
+            }
+            if(isInGroup(student)) {
+                System.out.println("Student jest juz w tej grupie");
+            }
+            e.printStackTrace();
+        }
+        return studentsNew;
+    }
+}
