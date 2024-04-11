5215a8100e4ce9f8b534a9277d476dee16a44db0
s30325 <162446731+s30325@users.noreply.github.com>
2024-04-11 14:03:21 +0200

    Update StudentGroup.java

--- untitled/src/StudentGroup.java
+++ untitled/src/StudentGroup.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
-public class StudentGroup {
-    public String nazwa;
-    ArrayList<Student> students=new ArrayList<Student>();
+    public class StudentGroup {
+        public String nazwa;
+        ArrayList<Student> students=new ArrayList<Student>();
 
-    public void AddStudent(Student student) throws Exception{
-        if(students.size() < 15) {
-            this.students.add(student);}
-        else{
-            throw new IllegalArgumentException();
-        }
-        if(students.contains(student)){
-            throw new IllegalArgumentException();
+        public void AddStudent(Student student) throws Exception{
+            if(students.size() < 15) {
+                this.students.add(student);}
+            else{
+                throw new IllegalArgumentException("Za duza liczba studentow w tej grupie");
+            }
+            if(students.contains(student)){
+                throw new IllegalArgumentException("Ten student jest już w tej grupie");
+            }
         }
     }
-}
